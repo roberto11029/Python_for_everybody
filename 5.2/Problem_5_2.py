896033a9cd2ecd598b6d0aa6f29ca7768d338223
Miguel <machavezg@icloud.com>
2018-11-06 23:34:17 -0800

    Update Problem_5_2.py

--- 5.2/Problem_5_2.py
+++ 5.2/Problem_5_2.py
@@ -2,14 +2,15 @@ largest = None
 smallest = None
 while True:
     try:
-        num = input("Enter a number: ")
-        if num == "done" : break
-        n= int(num)
-        largest = num if largest<num or largest == None else largest
-        smallest = num if smallest>num or smallest == None else smallest
+        num = raw_input('Enter a number: ')
+        if num == 'done' :
+            break
+        n = int(num)
+        largest = n if largest < n or largest == None else largest
+        smallest = n if smallest > n or smallest == None else smallest
     except:
-        print ("Invalid input")
+        print ('Invalid input')
     
 
-print("Maximum", largest)
-print ("Minimum", smallest)
+print('Maximum', largest)
+print ('Minimum', smallest)
